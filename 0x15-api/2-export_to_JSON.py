a4f6f27f546c37fe13cfe3af3c19ee2a8f21977c
Wise4lyf <wisdoms85@yahoo.com>
2023-05-22 18:32:30 -0700

    update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python Script that exports to-do list info for a given employee ID to JSON format."""
+"""Exports to-do list info for a given employee ID to JSON format."""
 import json
 import requests
 import sys
